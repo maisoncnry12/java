5f455a86cae843a82ece7901f1d6a0012f2c6eb6
maisoncnry12 <caxxry@gmail.com>
2022-02-01 18:10:09 +0900

    first commit

---
+++ wcp/Ready01.java
@@ -0,0 +1,11 @@
+public class Ready01 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+// public class Ready01 {
+// public static void main(String[] args) {
+// System.out.println("Hello World!");
+// }
+// }
